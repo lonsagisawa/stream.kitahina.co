9b98c2e8c144a591bd6cbf3ab21054ce11a49b42
Lon Sagisawa <lon@sagisawa.me>
2022-12-22 00:26:34 +0900

    add platform selector

--- pages/album/[id].tsx
+++ pages/album/[id].tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import Header from "../../components/header";
@@ -36,14 +37,99 @@ const Album = ({ albums }: any) => {
 
   const album = getAlbum(id);
 
+  class Player extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        enabled: "spotify",
+      };
+    }
+    enableSpotify = () => {
+      this.setState({ enabled: "spotify" });
+    };
+    enableAppleMusic = () => {
+      this.setState({ enabled: "applemusic" });
+    };
+    enableAmazon = () => {
+      this.setState({ enabled: "amazon" });
+    };
+    render() {
+      if (this.state.enabled == "spotify") {
+        return (
+          <>
+            <div className="flex flex-row my-2">
+              <div
+                className="border-b-2 border-green-500 p-2 cursor-pointer"
+                onClick={this.enableSpotify}
+              >
+                Spotify
+              </div>
+              <div
+                className="p-2 cursor-pointer"
+                onClick={this.enableAppleMusic}
+              >
+                Apple Music
+              </div>
+              <div className="p-2 cursor-pointer" onClick={this.enableAmazon}>
+                Prime Music
+              </div>
+            </div>
+            <Spotify id={album.spotify} />
+          </>
+        );
+      } else if (this.state.enabled == "applemusic") {
+        return (
+          <>
+            <div className="flex flex-row my-2">
+              <div className="p-2 cursor-pointer" onClick={this.enableSpotify}>
+                Spotify
+              </div>
+              <div
+                className="border-b-2 border-rose-500 p-2 cursor-pointer"
+                onClick={this.enableAppleMusic}
+              >
+                Apple Music
+              </div>
+              <div className="p-2 cursor-pointer" onClick={this.enableAmazon}>
+                Prime Music
+              </div>
+            </div>
+            <AppleMusic id={album.applemusic} />
+          </>
+        );
+      } else if (this.state.enabled == "amazon") {
+        return (
+          <>
+            <div className="flex flex-row my-2">
+              <div className="p-2 cursor-pointer" onClick={this.enableSpotify}>
+                Spotify
+              </div>
+              <div
+                className="p-2 cursor-pointer"
+                onClick={this.enableAppleMusic}
+              >
+                Apple Music
+              </div>
+              <div
+                className="border-b-2 border-orange-500 p-2 cursor-pointer"
+                onClick={this.enableAmazon}
+              >
+                Prime Music
+              </div>
+            </div>
+            <Amazon id={album.amazon} />
+          </>
+        );
+      }
+    }
+  }
+
   return (
     <div className="container mx-auto">
       <Header />
       <div>
         <h2 className="text-xl font-bold">{album.name}</h2>
-        <Spotify id={album.spotify} />
-        <AppleMusic id={album.applemusic} />
-        <Amazon id={album.amazon} />
+        <Player />
       </div>
     </div>
   );
