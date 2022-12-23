5d6324c1789b14677d068107290e662d78511541
Lon Sagisawa <lon@sagisawa.me>
2022-12-23 22:12:13 +0900

    header style

--- components/header.tsx
+++ components/header.tsx
@@ -7,36 +7,36 @@ interface headerProps {
 const BrandTitle = ({ brand }: headerProps) => {
   if (brand == "allstars") {
     return (
-      <Link href="/allstars" className="text-3xl ml-3">
+      <Link href="/allstars" className="text-3xl font-bold">
         765PRO ALLSTARS
       </Link>
     );
   } else if (brand == "cinderellagirls") {
     return (
-      <Link href="/cinderellagirls" className="text-3xl ml-3">
+      <Link href="/cinderellagirls" className="text-3xl font-bold">
         Cinderella Girls
       </Link>
     );
   } else if (brand == "millionlive") {
     return (
-      <Link href="/millionlive" className="text-3xl ml-3">
+      <Link href="/millionlive" className="text-3xl font-bold">
         Million Live
       </Link>
     );
   } else if (brand == "sidem") {
     return (
-      <Link href="/sidem" className="text-3xl ml-3">
+      <Link href="/sidem" className="text-3xl font-bold">
         SideM
       </Link>
     );
   } else if (brand == "shinycolors") {
     return (
-      <Link href="/shinycolors" className="text-3xl ml-3">
+      <Link href="/shinycolors" className="text-3xl font-bold">
         Shiny Colors
       </Link>
     );
   } else {
-    return <span className="text-3xl ml-3"></span>;
+    return <span className="text-3xl font-bold"></span>;
   }
 };
 
@@ -47,8 +47,8 @@ const Header = ({ brand }: headerProps) => {
         <Link href="/" className="text-4xl hover:border-b-2 border-amber-300">
           STREAM@S
         </Link>
-        <BrandTitle brand={brand} />
       </h1>
+      <BrandTitle brand={brand} />
     </header>
   );
 };
