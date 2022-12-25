dc20321bd4c4353d2c245902c88faa90c1279edb
Lon Sagisawa <lon@sagisawa.me>
2022-12-26 02:12:46 +0900

    footer styling

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,13 +1,13 @@
 const Footer = () => {
   return (
-    <footer className="container px-4 py-4 mx-auto max-w-2xl text-sm text-gray-500">
+    <footer className="container px-4 py-4 mx-auto mt-4 max-w-2xl text-sm text-slate-400 dark:text-slate-600 border-t border-slate-400 dark:border-slate-600">
       <p>
         こちらは有志により作成されたファンサイトです
         <br />
         このWebサイトについてランティス様/日本コロムビア様/その他関係各所へのお問い合わせはご遠慮ください
         <br />
-        <a href="https://kitahina.co" className="underline">
-          kitahina.co
+        <a href="https://kitahina.co" className="underline font-bold">
+          ← kitahina.co
         </a>
       </p>
     </footer>
