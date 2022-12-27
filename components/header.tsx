bb5f2c171e6e6998325249069148f88d183409f7
Lon Sagisawa <lon@sagisawa.me>
2022-12-28 02:23:20 +0900

    header enhance

--- components/header.tsx
+++ components/header.tsx
@@ -7,31 +7,46 @@ interface headerProps {
 const BrandTitle = ({ brand }: headerProps) => {
   if (brand == "allstars") {
     return (
-      <Link href="/allstars" className="text-3xl font-bold">
+      <Link
+        href="/allstars"
+        className="text-3xl font-bold border-b-2 border-transparent transition hover:border-rose-500"
+      >
         765PRO ALLSTARS
       </Link>
     );
   } else if (brand == "cinderellagirls") {
     return (
-      <Link href="/cinderellagirls" className="text-3xl font-bold">
+      <Link
+        href="/cinderellagirls"
+        className="text-3xl font-bold border-b-2 border-transparent transition hover:border-blue-500"
+      >
         Cinderella Girls
       </Link>
     );
   } else if (brand == "millionlive") {
     return (
-      <Link href="/millionlive" className="text-3xl font-bold">
+      <Link
+        href="/millionlive"
+        className="text-3xl font-bold border-b-2 border-transparent transition hover:border-yellow-500"
+      >
         Million Live
       </Link>
     );
   } else if (brand == "sidem") {
     return (
-      <Link href="/sidem" className="text-3xl font-bold">
+      <Link
+        href="/sidem"
+        className="text-3xl font-bold border-b-2 border-transparent transition hover:border-emerald-500"
+      >
         SideM
       </Link>
     );
   } else if (brand == "shinycolors") {
     return (
-      <Link href="/shinycolors" className="text-3xl font-bold">
+      <Link
+        href="/shinycolors"
+        className="text-3xl font-bold border-b-2 border-transparent transition hover:border-sky-500"
+      >
         Shiny Colors
       </Link>
     );
@@ -44,16 +59,28 @@ const Header = ({ brand }: headerProps) => {
   return (
     <header className="py-8">
       <h1 className="font-black">
-        <Link href="/" className="text-4xl hover:border-b-2 border-amber-300">
+        <Link
+          href="/"
+          className="text-4xl transition border-b-2 border-transparent hover:border-amber-300"
+        >
           STREAM@S
         </Link>
       </h1>
       <BrandTitle brand={brand} />
-      <p>
-        <Link href="/about" className="underline">
+      <div className="flex flex-col md:flex-row mt-2 gap-2">
+        <Link
+          href="/about"
+          className="underline hover:text-amber-400 transition"
+        >
           STREAM@Sとは？
         </Link>
-      </p>
+        <Link
+          href="/info"
+          className="underline hover:text-amber-400 transition"
+        >
+          お知らせ
+        </Link>
+      </div>
     </header>
   );
 };
