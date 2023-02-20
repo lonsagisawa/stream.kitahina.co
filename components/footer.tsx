b106e679e492f1e10072882d753c89bbc8a3af0a
Lon Sagisawa <lon@sagisawa.me>
2023-02-20 19:11:03 +0900

    replace tailwind style in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,36 +1,20 @@
 import Link from "next/link";
+import styles from "./footer.module.css";
 
 const Footer = () => {
   return (
-    <footer className="container px-4 py-4 mx-auto mt-4 max-w-2xl text-sm text-slate-400 dark:text-slate-600 border-t border-slate-400 dark:border-slate-600">
+    <footer className={styles.footer}>
       <p>
-        <div className="flex flex-col lg:flex-row my-2 gap-2 lg:gap-4">
-          <Link
-            href="/about"
-            className="underline hover:text-amber-400 transition"
-          >
-            STREAM@Sとは？
-          </Link>
-          <Link
-            href="/privacy"
-            className="underline hover:text-amber-400 transition"
-          >
-            プライバシーポリシー
-          </Link>
-          <Link
-            href="https://twitter.com/kitahinaCo"
-            className="underline hover:text-amber-400 transition"
-          >
-            Twitter
-          </Link>
-        </div>
+        <nav className={styles.footer_nav}>
+          <Link href="/about">STREAM@Sとは？</Link>
+          <Link href="/privacy">プライバシーポリシー</Link>
+          <Link href="https://twitter.com/kitahinaCo">Twitter</Link>
+        </nav>
         こちらは有志により作成されたファンサイトです
         <br />
         このWebサイトについてランティス様/日本コロムビア様/その他関係各所へのお問い合わせはご遠慮ください
         <br />
-        <a href="https://kitahina.co" className="underline font-bold">
-          ← kitahina.co
-        </a>
+        <a href="https://kitahina.co">← kitahina.co</a>
       </p>
     </footer>
   );
